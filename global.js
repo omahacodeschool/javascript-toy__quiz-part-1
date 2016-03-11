54952640a6a89e430018733c1fb00d1eb6a27e3a
Derek Pressnall <derekpressnall@yahoo.com>
2016-03-11 11:10:53 -0600

    wip on loop style

--- global.js
+++ global.js
@@ -8,31 +8,20 @@ window.onload = function(){
 
   button.addEventListener("click", function(){
 
-    // var question = document.getElementsByClassName("question");
+    var question = document.getElementsByClassName("question");
 
-    // for (var i = 0; i < question.length; i++) {
-    //   question[i]
-    // }
-
-    var q1 = prompt("At this very moment, which do I prefer?\n 1. red\n 2. blue\n 3. green\n 4. orange", "Type the number of your answer here");
-    if (q1 != answer1){
+    for (var i = 0; i < question.length; i++) {
+      prompt(question[i]);
+      if (q1 != answer1){
       alert("Sorry. You answer was incorrect."); 
-    } else {
+      } else {
       alert("That's right!"); count++;
-    }
-    var q2 = prompt("Which would I rather do?\n 1. sleep\n 2. eat\n 3. talk\n 4. walk", "Type the number of your answer here");
-    if (q2 != answer2){
-      alert("Sorry. You answer was incorrect.");
-    } else {
-      alert("That's right!"); count++;
-    }
-    var q3 = prompt("Which sound do I perfer?\n 1. alarm\n 2. birds chirping\n 3. laughing\n 4. the crunch of snow", "Type the number of your answer here");
-    if (q3 != answer3){
-      alert("Sorry. You answer was incorrect.");
-    } else {
-      alert("That's right!"); count++;
-    }
+    } 
+
     
+    if (count == 0){
+      alert("You answered nothing correctly");
+    }
     if (count == 1){
       alert("You answered 1 of 3 correctly");
     }
