e553414a2fbe47b8d24ef1c9bc5b93ec92e0d660
Derek Pressnall <derekpressnall@yahoo.com>
2016-03-11 13:23:19 -0600

    wip

--- global.js
+++ global.js
@@ -1,38 +1,43 @@
 window.onload = function(){
 
-  var button = document.getElementById("begin_button")
-  var count = 0
+  
 
-  button.addEventListener("click", function(){
+  var questions = document.getElementById(".quiz .questions ").setAttribute("id", "democlass");
+  // questions.innerText == "What is my name?";
 
-    var question = document.getElementsByClassName("question");
-    var correct_answer = document.getElementsByClassName("answers");
+  // var button = document.getElementById("begin_button")
+  // var count = 0
 
-    for (var i = 0; i < question.length; i++) {
-      var guess = prompt(question[i]);
+  // button.addEventListener("click", function(){
 
-      for (var a = 0; a < correct_answer.length; a++)
-        var answer = correct_answer[a];
+  //   var question = document.getElementsByClassName("question");
+  //   var correct_answer = document.getElementsByClassName("answers");
+
+  //   for (var i = 0; i < question.length; i++) {
+  //     var guess = prompt(question[i]);
+
+  //     for (var a = 0; a < correct_answer.length; a++)
+  //       var answer = correct_answer[a];
         
-        if (guess != correct_answer){
-        alert("Sorry. You answer was incorrect."); 
-        } else {
-        alert("That's right!"); count++;
-      }
-    } 
+  //       if (guess != correct_answer){
+  //       alert("Sorry. You answer was incorrect."); 
+  //       } else {
+  //       alert("That's right!"); count++;
+  //     }
+  //   } 
 
     
-    if (count == 0){
-      alert("You answered nothing correctly");
-    }
-    if (count == 1){
-      alert("You answered 1 of 3 correctly");
-    }
-    if (count == 2){
-      alert("You answered 2 of 3 correctly");
-    }
-    if (count == 3){
-      alert("You answered 3 of 3 correctly");
-    }
-  });
+  //   if (count == 0){
+  //     alert("You answered nothing correctly");
+  //   }
+  //   if (count == 1){
+  //     alert("You answered 1 of 3 correctly");
+  //   }
+  //   if (count == 2){
+  //     alert("You answered 2 of 3 correctly");
+  //   }
+  //   if (count == 3){
+  //     alert("You answered 3 of 3 correctly");
+  //   }
+  // });
 };
