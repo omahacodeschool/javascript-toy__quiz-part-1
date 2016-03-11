6e2a3c077360a85fdfefc055d5d9f66f02e7043e
Melinda Deinert <melinda.deinert@gmail.com>
2016-03-11 14:04:27 -0600

    If statement in question code working

--- global.js
+++ global.js
@@ -4,6 +4,7 @@ window.onload = function(){
   var question = document.getElementById('question');
   var choices = document.getElementById('choices');
   var answer = document.getElementById('answer');
+  var enter = document.getElementById('response');
   var question_result = document.getElementById('question_result');
   var next = document.getElementById('next');
   var total = document.getElementById('total_result');
@@ -17,10 +18,22 @@ window.onload = function(){
   button.addEventListener("click", function() {
 
     button.style.display = "none";
-
     question.innerHTML = icelandHash["question"];
     choices.innerHTML = icelandHash["choices"];
+    answer.value = "";
     quiz.style.display = "block";
+    enter.style.display = "block";
+
+
+    enter.addEventListener("click", function() {
+      var params = answer.value
+      if (params == icelandHash["capital"]) {
+        points_count++
+        console.log("YAAAAAAAY! points_count is " + points_count)
+      } else {
+        console.log("Incorrect answer.")
+      }
+    });
 
   });
 
