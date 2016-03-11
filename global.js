9f72d369cf8084500def72a9bdb312e28ea30003
Shannon Jackson <shannonfromomaha@gmail.com>
2016-03-11 12:00:16 -0600

    knows the right answer, informs user, adds to score counter

--- global.js
+++ global.js
@@ -1,5 +1,5 @@
 window.onload = function(){
-  //first just make ONE question that displays properly
+  //function for shuffling array, NOT MINE
   function fischer_yates_shuffle(array) {
     var m = array.length, t, i;
     while (m) {
@@ -9,17 +9,37 @@ window.onload = function(){
       array[i] = t;
     }
   }
+  // let's count how many answers they get correct. starts at 0 obvs 
+  right_answers = 0
+  //set up test question and choices, randomizes choices array
   var question1 = "How great is pizza?";
   var answer = "SUPER GREAT";
   var notanswers = ["great","fine","i've never had pizza"];
   notanswers.push(answer)
   fischer_yates_shuffle(notanswers)
-  var choices = document.querySelectorAll(".choices li");
+  //when "next" is clicked it replaces question and answers
   var next = document.getElementById("next");
   next.addEventListener("click", function() {
     document.getElementById("question").innerHTML = question1;
+    var choices = document.querySelectorAll(".choices li");
     for (var i = choices.length - 1; i >= 0; i--) {
       choices[i].innerHTML = notanswers[i]
     }
   });
+  //if they input the correct answer it tells them they are right
+  var submit_guess = document.getElementById("submitter")
+  submit_guess.addEventListener("click", function() {
+    guess = document.getElementById("answer").value;
+    if (guess === answer) {
+      right_answers++;
+      console.log("right_answers = " + right_answers);
+      document.getElementById("question_result").innerHTML = "CORRECT!";
+    };
+  });
 };
+
+
+
+
+
+
