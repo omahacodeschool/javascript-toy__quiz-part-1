d0f44f281aec610c1f7ad6e2d5c80952a77e47ef
Shannon Jackson <shannonfromomaha@gmail.com>
2016-03-11 10:35:55 -0600

    hm. my choices are gone? let's investigate.

--- global.js
+++ global.js
@@ -1,8 +1,12 @@
 window.onload = function(){
   //first just make ONE question that displays properly
   var question1 = "How great is pizza?";
+  var choices = document.getElementById("next");
   var next = document.getElementById("next");
   next.addEventListener("click", function() {
     document.getElementById("question").innerHTML = question1;
+    for (var i = choices.length - 1; i >= 0; i--) {
+      choices[i].innerHTML = "GREAT!"
+    }
   });
 };
