dfc1da53815095118914e1af5ba75c157b3d8a69
Thorin Butler <thorin.butler@gmail.com>
2016-03-11 15:27:41 -0600

    Got the button to start the game and ask a question

--- global.js
+++ global.js
@@ -1,3 +1,17 @@
 window.onload = function(){
+
+  var button = document.getElementById("begin_button");
+
+  button.addEventListener("click", function(){
+
+    var question = prompt("What weighs more? \n A)A feather \n B)An elephant \n C)The moon");
+  })
+
   alert("Foo");
 };
+
+/*1. Your user loads the **index.html** page, which contains a button to begin the game.
+2. They are presented with a `prompt()` asking them a multiple choice question.
+3. Upon answer, they see an `alert()` telling them whether they answered correctly or incorrectly. They click "Okay" to move on.
+4. Steps 2-3 repeat until there are no more questions.
+5. Once they have answered all questions, they see an `alert()` telling them how many they answered correctly (out of however many questions there were), and something that looks like a percentage. E.g. **You answered 7 of 10 questions correctly, good for 70.0%**/
