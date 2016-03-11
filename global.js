a4e5ecfc81e912247bda6f25c903776270a146d8
Shannon Jackson <shannonfromomaha@gmail.com>
2016-03-11 10:59:37 -0600

    Is there a way to loop through TWO arrays at once? In my js, line 11, I'd like to take the array of choices and reassign them as the non-answers. Is that possible or am I overthinking it? should I just write out "choice1 = notanswer[1]"?

--- global.js
+++ global.js
@@ -3,10 +3,11 @@ window.onload = function(){
   var question1 = "How great is pizza?";
   var answer = "SUPER GREAT";
   var notanswers = ["great","fine","i've never had pizza"];
+  notanswers.push(answer)
   var choices = document.querySelectorAll(".choices li");
   var next = document.getElementById("next");
   next.addEventListener("click", function() {
-    document.getElementById("question").innerHTML = question1;
+    //document.getElementById("question").innerHTML = question1;
     for (var i = choices.length - 1; i >= 0; i--) {
       choices[i].innerHTML = "GREAT!"
       //can you loop through multiple arrays??
