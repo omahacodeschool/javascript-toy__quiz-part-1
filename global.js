60ca8d8b23583a40dc97399c66cf0c0e5c5db765
Shannon Jackson <shannonfromomaha@gmail.com>
2016-03-11 10:43:23 -0600

    changes all choices. still have to figure out nesting issue

--- global.js
+++ global.js
@@ -1,7 +1,7 @@
 window.onload = function(){
   //first just make ONE question that displays properly
   var question1 = "How great is pizza?";
-  var choices = document.getElementById("next");
+  var choices = document.querySelectorAll(".choices li");
   var next = document.getElementById("next");
   next.addEventListener("click", function() {
     document.getElementById("question").innerHTML = question1;
