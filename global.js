44dc8d035d9ec15e26f57e4540af7090c030369b
Jeremy Stoll <j8r8my@gmail.com>
2016-03-11 14:41:07 -0600

    Working busily

--- global.js
+++ global.js
@@ -1,67 +1,79 @@
 window.onload = function(){
   //alert("Welcome!");
 
-function runQuiz(){
+  function runQuiz(){
 
-//set score to zero
-userScore = 0
+  //set score to zero
+  userScore = 0
 
-//get questions
-getQuestions()
+  //get questions
+  getQuestions()
 
-//run all questions
-runAllQuestions()
+  //run all questions
+  runAllQuestions()
 
-//get final score
-getFinalScore()
+  //get final score
+  getFinalScore()
 
-}
-
-
-function getQuestions(){
-
-  // Sets each question to an array containing 6 strings, each element of which is to be referenced by index:
-  // array[0] = The question 
-  // array[1] = Guess A
-  // array[2] = Guess B
-  // array[3] = Guess C
-  // array[4] = Guess D
-  // array[5] = The answer (A, B, C, or D)
-
-  var question1 = ["How many cats are too many?", "1", "2", "3", "No quantity of cats will completely fill my heart to cat-capacity", "D"]
-
-  var question2 = ["What color is patience?", "Blue", "Green", "Puce", "Orange", "B"]
-
-  var question3 = ["Do androids dream of electric sheep?", "Always", "Yes", "Most Likely", "Mercer demands actual, living dream-sheep.", "D"]
-
-  var question4 = ["What is love?", "Coffee", "Kindness", "Agony", "Worship", "A"]
-}
-
-function runAllQuestions(){
-  
-  
-  currentQuestion
-  runQuestion()
-  setScore()
-}
-
-function runQuestion(){
+  }
 
 
-}
+  function getQuestions(){
 
-function setScore(){
+    // Sets each question to an array containing 6 strings, each element of which is to be referenced by index:
+    // array[0] = The question 
+    // array[1] = Guess A
+    // array[2] = Guess B
+    // array[3] = Guess C
+    // array[4] = Guess D
+    // array[5] = The answer (A, B, C, or D)
+    
+
+    var question1 = ["How many cats are too many?", "1", "2", "3", "No quantity of cats will ever completely fill my heart to cat-capacity", "D"]
+
+    var question2 = ["What color is patience?", "Blue", "Green", "Puce", "Orange", "B"]
+
+    var question3 = ["Do androids dream of electric sheep?", "Always", "Yes", "Most Likely", "Mercer demands actual, living dream-sheep.", "D"]
+
+    var question4 = ["What is love?", "Coffee", "Kindness", "Agony", "Worship", "A"]
+
+    // Set the array allQuestionsArray to contain all the questions supplied above. For now, this is done manually.
+    allQuestionsArray = [question1, question2, question3, question4]
+
+  }
+
+  function runAllQuestions(){
+
+    for (var i = -1; i < allQuestionsArray.length; i++) {
+      var currentQuestion = allQuestionsArray[i][0] 
+      var currentGuess1 = allQuestionsArray[i][1]
+      var currentGuess2 = allQuestionsArray[i][2]
+      var currentGuess3 = allQuestionsArray[i][3]
+      var currentGuess4 = allQuestionsArray[i][4]
+      var currentAnswer = allQuestionsArray[i][5]
+
+    }
+    
+    setScore()
+  }
+
+  function runQuestion(q){
 
 
-}
+  }
 
-function getFinalScore(){
+  function setScore(){
 
 
-}
+  }
 
-startButton = getElementById("start_button")
-startButton.addEventListener("click", runQuiz())
+  function getFinalScore(){
+
+
+  }
+
+  startButton = getElementById("start_button")
+  startButton.addEventListener("click", runQuiz())
 
 
 
