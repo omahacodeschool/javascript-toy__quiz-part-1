da0455dfd27f9fe8e2ba3b0d3a4bfa88ef44f25e
Beth Haubert <bethany.haubert@gmail.com>
2016-03-11 16:53:21 -0600

    trying to randomize answers

--- global.js
+++ global.js
@@ -1,18 +1,19 @@
 window.onload = function(){
 
-var hashQuestionsAndAnswers = {};
-  hashQuestionsAndAnswers["What's the best cat name?"] = ["Xiao Gui", "Clementine", "Kitten", "Gita"];
-  hashQuestionsAndAnswers["What's the ideal number of kitties?"] = ["all the kitties", 1, 2, 3];
-  hashQuestionsAndAnswers["What is the best part about kitties?"] = ["Their furry bellies", "Their jellybean paws", "Their silly meows", "Their twitchy tails"];
-  
-var quiz = document.getElementById("quiz");
-var begin_button = document.getElementById("begin_button");
-var question_text = document.getElementById("question");
-var choices = document.getElementById("choices");
-var answer = document.getElementById("answer");
-var submit_button = document.getElementById("submitter");
-var next_button = document.getElementById("next");
-var result = document.getElementById("total_result");
+  var hashQuestionsAndAnswers = {};
+    hashQuestionsAndAnswers["What's the best cat name?"] = ["Xiao Gui", "Clementine", "Kitten", "Gita"];
+    hashQuestionsAndAnswers["What's the ideal number of kitties?"] = ["all the kitties", 1, 2, 3];
+    hashQuestionsAndAnswers["What is the best part about kitties?"] = ["Their furry bellies", "Their jellybean paws", "Their silly meows", "Their twitchy tails"];
+    
+  var quiz = document.getElementById("quiz");
+  var begin_button = document.getElementById("begin_button");
+  var question_text = document.getElementById("question");
+  var choices = document.getElementById("choices");
+  var answer = document.getElementById("answer");
+  var submit_button = document.getElementById("submitter");
+  var next_button = document.getElementById("next");
+  var result = document.getElementById("total_result");
+  var correct_answers = 0;
 
 
   begin_button.addEventListener("click", function(){
@@ -20,36 +21,40 @@ var result = document.getElementById("total_result");
     begin_button.style.display = "none";
     quiz.style.display = "block";
 
-    for (var key in hashQuestionsAndAnswers){
-      question_text.innerHTML = key;
-      choices.innerHTML = hashQuestionsAndAnswers[key];
-      var correct_answers = 0;
+    // for (var key in hashQuestionsAndAnswers){
+    //   question_text.innerHTML = key;
+    //   choices.innerHTML = hashQuestionsAndAnswers[key];
+    //   break;
+
+    var hash_key = Object.keys(hashQuestionsAndAnswers)[0];
+      question_text.innerHTML = hash_key;
+      
+      choices.innerHTML = hashQuestionsAndAnswers[hash_key].sort(function() { return 0.5 - Math.random() });
 
       submit_button.addEventListener("click", function(){
 
         if (answer == hashQuestionsAndAnswers[key][0]){
-          next_button.display = "block";
           next_button.addEventListener("click", function(){
             alert("You answered correctly");
-            correct_answers ++ ;
+            correct_answers += 1 ;
           });
-        };
+        }
 
         else {
-          next_button.display = "block";
           next_button.addEventListener("click", function(){
             alert("WRONG. The correct answer is " + hashQuestionsAndAnswers[key][0]);
-            };
+            });
         };
 
       });
       
-    };
+    });
 
-    result.innerHTML = ("You got" + correct_answers + "out of" + Object.keys(hashQuestionsAndAnswers).length + "answers correct");
-    result.display = "block";
-  });
-};
+    // result.innerHTML = ("You got" + correct_answers + "out of" + Object.keys(hashQuestionsAndAnswers).length + "answers correct");
+    // result.display = "block";
+
+  };
+
 
 
 
