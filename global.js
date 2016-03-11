581650ae21edc7d09abc3c4d58ec620f1134c53d
Shannon Jackson <shannonfromomaha@gmail.com>
2016-03-11 10:27:59 -0600

    got it to change the question with 'next'

--- global.js
+++ global.js
@@ -1,3 +1,8 @@
 window.onload = function(){
-  alert("Foo");
+  //first just make ONE question that displays properly
+  var question1 = "How great is pizza?";
+  var next = document.getElementById("next");
+  next.addEventListener("click", function() {
+    document.getElementById("question").innerHTML = question1;
+  });
 };
