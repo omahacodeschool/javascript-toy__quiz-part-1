de6a9baecf940026157ce63f432a773899a02a87
Melinda Deinert <melinda.deinert@gmail.com>
2016-03-11 11:32:27 -0600

    Got question and choices for first question to appear

--- global.js
+++ global.js
@@ -1,61 +1,78 @@
 window.onload = function(){
-  var points_count = 0
+  var points_count = 0;
+  var question = document.getElementById('question');
+  var choices = document.getElementById('choices');
+  var answer = document.getElementById('answer');
+  var question_result = document.getElementById('question_result');
+  var next = document.getElementById('next');
+  var total = document.getElementById('total_result');
 
-  var button = document.getElementById('begin_button')
+  var icelandHash = new Object();
+    icelandHash["question"] = "What's the capital of Iceland?";
+    icelandHash["choices"] = ["Oslo, Torshavn, Reykjavik, or Helsinki"];
+    icelandHash["capital"] = "Reykjavik";
+
+
+  var button = document.getElementById('begin_button');
   button.addEventListener("click", function() {
 
-    var iceland = prompt("What's the capital of Iceland?\nOslo\nTorshavn\nReykjavik\nHelsinki")
-      var correct_answer = "Reykjavik"
+  var quiz = document.getElementById('quiz');
+  question.innerHTML = icelandHash["question"]
+  choices.innerHTML = icelandHash["choices"]
+  quiz.style.display = "block";
 
-      if (correct_answer == iceland) {
-        points_count++
-        alert("You answered correctly! You have " + points_count + " points.")
-      } else {
-      alert("Sorry, that was incorrect. You have " + points_count + " points.")
-    }
+    // var iceland = prompt("What's the capital of Iceland?\nOslo\nTorshavn\nReykjavik\nHelsinki")
+    //   var correct_answer = "Reykjavik"
+
+    //   if (correct_answer == iceland) {
+    //     points_count++
+    //     alert("You answered correctly! You have " + points_count + " points.")
+    //   } else {
+    //   alert("Sorry, that was incorrect. You have " + points_count + " points.")
+    // }
 
 
-    var jordan = prompt("What's the capital of Jordan?\nJerusalem\nTel Aviv\nAmman\nCairo")
-      var correct_answer = "Amman"
+    // var jordan = prompt("What's the capital of Jordan?\nJerusalem\nTel Aviv\nAmman\nCairo")
+    //   var correct_answer = "Amman"
 
-      if (correct_answer == jordan) {
-        points_count++
-        alert("You answered correctly! You have " + points_count + " points.")
-      } else {
-        alert("Sorry, that was incorrect. You have " + points_count + " points.")
-      }
+    //   if (correct_answer == jordan) {
+    //     points_count++
+    //     alert("You answered correctly! You have " + points_count + " points.")
+    //   } else {
+    //     alert("Sorry, that was incorrect. You have " + points_count + " points.")
+    //   }
 
-    var brazil = prompt("What's the capital of Brazil?\nSao Paolo\nBuenos Aires\nRio de Janeiro\nBrasilia")
-      var correct_answer = "Brasilia"
+    // var brazil = prompt("What's the capital of Brazil?\nSao Paolo\nBuenos Aires\nRio de Janeiro\nBrasilia")
+    //   var correct_answer = "Brasilia"
 
-      if (correct_answer == brazil) {
-        points_count++
-        alert("You answered correctly! You have " + points_count + " points.")
-      } else {
-        alert("Sorry, that was incorrect. You have " + points_count + " points.")
-      }
+    //   if (correct_answer == brazil) {
+    //     points_count++
+    //     alert("You answered correctly! You have " + points_count + " points.")
+    //   } else {
+    //     alert("Sorry, that was incorrect. You have " + points_count + " points.")
+    //   }
 
-    var albania = prompt("What's the capital of Albania?\nPristina\nTirana\nBelgrade\nSkopje")
-      var correct_answer = "Tirana"
+    // var albania = prompt("What's the capital of Albania?\nPristina\nTirana\nBelgrade\nSkopje")
+    //   var correct_answer = "Tirana"
 
-      if (correct_answer == albania) {
-        points_count++
-        alert("You answered correctly! You have " + points_count + " points.")
-      } else {
-        alert("Sorry, that was incorrect. You have " + points_count + " points.")
-      }
+    //   if (correct_answer == albania) {
+    //     points_count++
+    //     alert("You answered correctly! You have " + points_count + " points.")
+    //   } else {
+    //     alert("Sorry, that was incorrect. You have " + points_count + " points.")
+    //   }
 
-    var turkey = prompt("What's the capital of Turkey?\nIstanbul\nIzmir\nSofia\nAnkara")
-      var correct_answer = "Ankara"
+    // var turkey = prompt("What's the capital of Turkey?\nIstanbul\nIzmir\nSofia\nAnkara")
+    //   var correct_answer = "Ankara"
 
-      if (correct_answer == turkey) {
-        points_count++
-        alert("You answered correctly! You have " + points_count + " points.")
-      } else {
-        alert("Sorry, that was incorrect. You have " + points_count + " points.")
-      }
+    //   if (correct_answer == turkey) {
+    //     points_count++
+    //     alert("You answered correctly! You have " + points_count + " points.")
+    //   } else {
+    //     alert("Sorry, that was incorrect. You have " + points_count + " points.")
+    //   }
 
-    alert("That's all. You have " + points_count + "/5 points! That's " + (points_count/5)*100 + "%.")
+  //   alert("That's all. You have " + points_count + "/5 points! That's " + (points_count/5)*100 + "%.")
 
-  })
+   })
 };
