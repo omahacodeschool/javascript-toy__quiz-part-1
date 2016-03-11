608660b11fb21c853826722e54d0e5dc4062fc51
Kyle Sandage <ksandage08@gmail.com>
2016-03-11 14:49:59 -0600

    storing functions as an array

--- global.js
+++ global.js
@@ -2,28 +2,46 @@ window.onload = function(){
   
   var startButton = document.getElementById("begin_button");
 
+  var allQuestions = {
+    questionOne: function(){
+      responseOne = prompt("What color are oranges? Blue? Green? Orange? Yellow?");
+      if (responseOne.toLowerCase() === "orange"){
+        alert("You are correct!");
+      }
+      else{
+        alert("WRONG!");
+      }
+    },
+    questionTwo: function(){
+      responseTwo = prompt("Who ran the first marathon? Kyle Sandage? Steve Prefontaine? Pheidippides? Usain Bolt?");
+      if (responseTwo.toLowerCase() === "pheidippides"){
+        alert("You are correct!");
+      }
+      else{
+        alert("WRONG!");
+      }
+    },
+    questionThree: function(){
+      responseThree = prompt("What is the capitol of Kansas? Wichita? Manhattan? Lawrence? Kansas City?");
+      if (responseThree.toLowerCase() === "wichita"){
+        alert("You are correct!");
+      }
+      else{
+        alert("WRONG!");
+      }
+    },
+  };
+
   beginGame = function(){
-    questionOne = prompt("What color are oranges? Blue? Green? Orange? Yellow?");
-    if (questionOne.toLowerCase() === "orange"){
-      alert("You are correct!")
-    }
-    else{
-      alert("WRONG!")
-    };
-    questionTwo = prompt("Who ran the first marathon? Kyle Sandage? Steve Prefontaine? Pheidippides? Usain Bolt?");
-    if (questionTwo.toLowerCase() === "pheidippides"){
-      alert("You are correct!")
-    }
-    else{
-      alert("WRONG!")
-    };
-    questionThree = prompt("What is the capitol of Kansas? Wichita? Manhattan? Lawrence? Kansas City?");
-    if (questionThree.toLowerCase() === "wichita"){
-      alert("You are correct!")
-    }
-    else{
-      alert("WRONG!")
-    };
+    allQuestions.questionOne();
+    allQuestions.questionTwo();
+    allQuestions.questionThree();
   };
   startButton.addEventListener("click", beginGame);
 };
+
+
+
+
+
+
