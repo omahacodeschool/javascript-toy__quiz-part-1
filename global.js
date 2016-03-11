3a9fc47bbdf9f56e7097697e77239059fe72640a
Kyle Sandage <ksandage08@gmail.com>
2016-03-11 13:46:59 -0600

    Two questions made

--- global.js
+++ global.js
@@ -1,3 +1,22 @@
 window.onload = function(){
-  alert("Foo");
+  
+  var startButton = document.getElementById("begin_button");
+
+  beginGame = function(){
+    questionOne = prompt("What color are oranges? Blue? Green? Orange? Yellow?");
+    if (questionOne.toLowerCase() === "orange"){
+      alert("You are correct!")
+    }
+    else{
+      alert("WRONG!")
+    };
+    questionTwo = prompt("Who ran the first marathon? Kyle Sandage? Steve Prefontaine? Pheidippides? Usain Bolt?");
+    if (questionTwo.toLowerCase() === "pheidippides"){
+      alert("You are correct!")
+    }
+    else{
+      alert("WRONG!")
+    };
+  };
+  startButton.addEventListener("click", beginGame);
 };
