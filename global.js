eed82c0beca4cfa48e5b9772d96af147d4bf1829
Tyler <tfisher322@gmail.com>
2016-03-11 10:21:04 -0600

    Have basic Question One functioning with wrong or right.

--- global.js
+++ global.js
@@ -1,4 +1,20 @@
 window.onload = function(){
-  alert("Foo");
+  var questOneAnswer = "cat"
+  
+  var click_it = document.getElementById("begin_button");
+  
+  click_it.addEventListener("click", function(){
+
+    var alert_to_show = alert("Welcome to the Quiz!");
+
+    var questOne = prompt("Question 1! What's the best pet to have? Dog, Cat, Ferret, or Mouse?");
+    if (questOne === questOneAnswer){ 
+      alert("Correct! Press OK to continue to question 2.");
+      
+    };
+    if (questOne != questOneAnswer){
+      alert("That's incorrect. Please click OK to continue to question 2.");
+    };
+      
+  });
 };
- 
