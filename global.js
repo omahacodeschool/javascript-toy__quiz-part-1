f567081c30dc32c5392bb50a51d77b446c13d5b3
Shannon Jackson <shannonfromomaha@gmail.com>
2016-03-11 10:48:53 -0600

    hm. HMMM.

--- global.js
+++ global.js
@@ -1,12 +1,15 @@
 window.onload = function(){
   //first just make ONE question that displays properly
   var question1 = "How great is pizza?";
+  var answer = "SUPER GREAT";
+  var notanswers = ["great","fine","i've never had pizza"];
   var choices = document.querySelectorAll(".choices li");
   var next = document.getElementById("next");
   next.addEventListener("click", function() {
     document.getElementById("question").innerHTML = question1;
     for (var i = choices.length - 1; i >= 0; i--) {
       choices[i].innerHTML = "GREAT!"
+      //can you loop through multiple arrays??
     }
   });
 };
