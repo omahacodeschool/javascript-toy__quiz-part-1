be6fa60d6987a7dbb068a92bcdde5e9d9a3f4eb4
Thorin Butler <thorin.butler@gmail.com>
2016-03-11 16:07:29 -0600

    Oops, forgot to make sure each question had four choices.  Fixed that.

--- global.js
+++ global.js
@@ -6,7 +6,7 @@ window.onload = function(){
 
   button.addEventListener("click", function(){
 
-    var questionOne = prompt("What weighs more? \n A)A feather \n B)An elephant \n C)The moon");
+    var questionOne = prompt("What weighs more? \n A)A feather \n B)An elephant \n C)The moon \n D)Nothing");
 
       if (questionOne == "c"){
         alert("That's correct!");
@@ -16,7 +16,7 @@ window.onload = function(){
         alert("Aww, better luck with the next one.");
       };
 
-    var questionTwo = prompt("What weighs less? \n A)A bowling ball \n B)A neutron star \n C)A puppy");
+    var questionTwo = prompt("What weighs less? \n A)A bowling ball \n B)A neutron star \n C)A puppy \n D)Your conscience");
 
       if (questionTwo == "c") {
         alert("That's correct!");
@@ -26,7 +26,7 @@ window.onload = function(){
         alert("Aww, better luck with the next one.");
       };
 
-    var questionThree = prompt("What weighs on you more? \n A)The atmosphere \n B)Debt \n C)Guilt");
+    var questionThree = prompt("What weighs on you more? \n A)The atmosphere \n B)Debt \n C)Guilt \n D)Responsibility");
 
       if (questionTwo == "c") {
         alert("That's correct!");
