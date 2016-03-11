b1c9ed92cd6b8475676848b93f2e99954c076664
Shannon Jackson <shannonfromomaha@gmail.com>
2016-03-11 11:33:20 -0600

    included the fischer-yates shuffle

--- global.js
+++ global.js
@@ -1,10 +1,19 @@
 window.onload = function(){
   //first just make ONE question that displays properly
-
+  function fischer_yates_shuffle(array) {
+    var m = array.length, t, i;
+    while (m) {
+      i = Math.floor(Math.random() * m--);
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+  }
   var question1 = "How great is pizza?";
   var answer = "SUPER GREAT";
   var notanswers = ["great","fine","i've never had pizza"];
   notanswers.push(answer)
+  fischer_yates_shuffle(notanswers)
   var choices = document.querySelectorAll(".choices li");
   var next = document.getElementById("next");
   next.addEventListener("click", function() {
