becc23c5e1c2285479ccdd6a65336e70ff34a14f
Ryan Newell <youcatastrophe@gmail.com>
2016-03-12 16:10:11 -0600

    Phase 2. Experimengint with single-question version. It works.

--- global.js
+++ global.js
@@ -1,3 +1,41 @@
-window.onload = function(){
-  alert("Foo");
-};
+window.onload = function() {
+
+  // user clicks begin button sto start quiz.
+  var begin = document.getElementById("begin_button");
+
+  begin.addEventListener("click", function() {
+
+    var question = document.getElementById("question");
+
+    question.innerHTML = ("How many hobbits were in 'The Fellowship' in The Lord of the Rings Trilogy?");
+
+    var choices = document.getElementById("choices");
+
+    var firstQuestionChoices = ("A. One <br> B. Two <br> C. Three <br> D. Four");
+
+    choices.innerHTML = (firstQuestionChoices);
+
+  });
+
+  // user submits answer
+  var submitted = document.getElementById('submitter');
+
+  submitted.addEventListener("click", function() {
+
+    var answer = document.getElementById("answer").value;
+    answer = answer.toUpperCase();
+
+    var correctAnswer = "D";
+
+    var questionResult = document.getElementById("questionResult");
+
+
+    if (answer == correctAnswer) {
+      questionResult.innerHTML = ("That is correct!");
+    } else {
+      questionResult.innerHTML = ("Sorry. " + correctAnswer + " is the answer!");
+    }
+
+  });
+ 
+};  
