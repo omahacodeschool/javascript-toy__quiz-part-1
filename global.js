2b1b17463ab146c98a2077d31fdba819c28b40f9
Ryan Newell <youcatastrophe@gmail.com>
2016-03-12 17:38:21 -0600

    Horrid. Unorganized chaos. But it's taking shape.

--- global.js
+++ global.js
@@ -1,41 +1,75 @@
 window.onload = function() {
 
+//ojbect for questions, choices, and correcct answers? Messy.
+   var quiz = {
+    firstQuestion: "How many hobbits were in 'The Fellowship' in The Lord of the Rings Trilogy?", 
+    secondQuestion: "What is the hobbit Pippin's last name?",
+    thirdQuestion: "What is the name of the person in the fellowship who tries to take the ring from Frodo?",
+    fourthQuestion: "What does the creature Gollum call the ring?",
+    firstQuestionChoices: "A. One <br> B. Two <br> C. Three <br> D. Four",
+    secondQuestionChoices: "A. Brandybuck <br> B. Took <br> C. Baggins <br> D.Gamgee",
+    thirdQuestionChoices: "A. Gandalf <br> B. Farimir <br> C. Borimir <br> D.Strider",
+    fourthQuestionChoices: "A. My Precious <br> B. My Treasure <br> C. My Ring <br> D. My Love",
+    firstCorrectAnswer: "D",
+    secondCorrectAnswer: "B",
+    thirdCorrectAnswer: "C",
+    fourthCorrectAnswer: "C" 
+  };  
+
+  var submitter = document.getElementById('submitter');
+
+
   // user clicks begin button sto start quiz.
-  var begin = document.getElementById("begin_button");
+  var begin = document.getElementById("beginButton");
+
+  var answerField = document.getElementById("answer");
+
 
   begin.addEventListener("click", function() {
 
+    begin.style.display = "none";
+
     var question = document.getElementById("question");
 
-    question.innerHTML = ("How many hobbits were in 'The Fellowship' in The Lord of the Rings Trilogy?");
+    question.innerHTML = (quiz.firstQuestion);
 
     var choices = document.getElementById("choices");
 
-    var firstQuestionChoices = ("A. One <br> B. Two <br> C. Three <br> D. Four");
+    choices.innerHTML = (quiz.firstQuestionChoices);
 
-    choices.innerHTML = (firstQuestionChoices);
+    submitter.style.display = "block";
+
+    answerField.style.display = "block";    
 
   });
 
   // user submits answer
-  var submitted = document.getElementById('submitter');
 
-  submitted.addEventListener("click", function() {
+  submitter.addEventListener("click", function() {
+
+    submitter.style.display = "none";
 
     var answer = document.getElementById("answer").value;
     answer = answer.toUpperCase();
 
-    var correctAnswer = "D";
+    var nextButton = document.getElementById("nextButton");
+    nextButton.style.display = "block";
 
-    var questionResult = document.getElementById("questionResult");
+    var questionResult = document.getElementById("questionResult"); 
 
-
-    if (answer == correctAnswer) {
+    if (answer == quiz.firstCorrectAnswer) {
       questionResult.innerHTML = ("That is correct!");
     } else {
-      questionResult.innerHTML = ("Sorry. " + correctAnswer + " is the answer!");
+      questionResult.innerHTML = ("Sorry. " + quiz.firstCorrectAnswer + " is the answer!");
     }
 
+    answerField.style.display = "none";
+
   });
+
+  submitter.addEventListener("click", function() { 
+    
+
+  });   
  
 };  
