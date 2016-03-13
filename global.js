09979bd5dd26f48bfb6336bb922dc389d82aa2c7
Jillian Waggoner <jillianwaggoner@icloud.com>
2016-03-13 18:59:41 -0500

    phase one -- one question functioning

--- global.js
+++ global.js
@@ -1,3 +1,19 @@
 window.onload = function(){
-  alert("Foo");
-}; 
+ 
+  var firstQuestionAns = "New York City"
+
+  var button = document.getElementById("begin_button");
+
+  button.addEventListener("click", function(){
+
+    var alert_to_show = alert("Quiz time!");
+
+    var firstQuestion = prompt("What was the first city to be illuminated with electric street lamps in 1882?");
+    if (firstQuestion === firstQuestionAns){ 
+      alert("That's correct! Click OK to continue.");
+    };
+    if (firstQuestion != firstQuestionAns){
+      alert("Sorry, that's incorrect. Click OK to continue.");
+    };
+  });
+};
