ab39da2a61c2fe85e028b4d6c97a7845b584831e
Meagan Jungman <meagan.jungman@gmail.com>
2016-03-13 16:06:55 -0500

    can now get first quiz in collection to display (and not the other quiz items)

--- global.js
+++ global.js
@@ -1,3 +1,18 @@
 window.onload = function(){
-  alert("Foo");
+  var buttonz = document.getElementById('buttonz');
+  var questions = document.getElementsByClassName('question');
+  var currentQuestion = 1;
+  var totalQuestions = questions.length;
+
+  buttonz.addEventListener("click", function changeButtonz() {
+    for (var i = 0; i < questions.length; i++) {
+     if (questions[i].id == "question" + currentQuestion) {
+        questions[i].style.display = "block";
+        buttonz.value = "Submit"
+      } else {
+        questions[i].style.display = "none";
+      }
+    }
+  });
+
 };
