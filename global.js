9a9eb7bbf5b80829b8e60c82deaef11c83d0d9ac
Jeremy Stoll <j8r8my@gmail.com>
2016-03-13 11:35:35 -0500

    Starting work on Sunday

--- global.js
+++ global.js
@@ -1,25 +1,28 @@
 window.onload = function(){
-  //alert("Welcome!");
+  console.log("loading")
+  alert("Welcome!");
+
+  // If adding to, or subtracting from, the number of questions, you will need to set the allQuestionsArray at the bottom of the getQuestions function to include or not include those question arrays.
 
   function runQuiz(){
+    console.log("runQuiz")
+    //set score to zero
+    var userScore = 0
 
-  //set score to zero
-  userScore = 0
+    //get questions
+    getQuestions()
 
-  //get questions
-  getQuestions()
+    //run all questions
+    runAllQuestions()
 
-  //run all questions
-  runAllQuestions()
-
-  //get final score
-  getFinalScore()
+    //get final score
+    getFinalScore()
 
   }
 
 
   function getQuestions(){
-
+    console.log("getQuestions")
     // Sets each question to an array containing 6 strings, each element of which is to be referenced by index:
     // array[0] = The question 
     // array[1] = Guess A
@@ -37,45 +40,64 @@ window.onload = function(){
 
     var question4 = ["What is love?", "Coffee", "Kindness", "Agony", "Worship", "A"]
 
-    // Set the array allQuestionsArray to contain all the questions supplied above. For now, this is done manually.
+    // Set the array `allQuestionsArray` to contain all the questions supplied above. For now, this is done manually.
     allQuestionsArray = [question1, question2, question3, question4]
 
   }
 
-  function runAllQuestions(){
 
-    for (var i = -1; i < allQuestionsArray.length; i++) {
+  function runAllQuestions(){
+    console.log("runAllQuestions")
+    var i = 0
+
+    while (i < allQuestionsArray.length){
+      
       var currentQuestion = allQuestionsArray[i][0] 
       var currentGuess1 = allQuestionsArray[i][1]
       var currentGuess2 = allQuestionsArray[i][2]
       var currentGuess3 = allQuestionsArray[i][3]
       var currentGuess4 = allQuestionsArray[i][4]
       var currentAnswer = allQuestionsArray[i][5]
+      
+      i++
+      console.log("createPrompt")
+    var userGuess = prompt(currentQuestion, currentGuess1, currentGuess2, currentGuess3, currentGuess4)
+      createPrompt()
 
-    }
+      assessGuess()
+
+      setScore()
     
-    setScore()
+    }
+
   }
 
-  function runQuestion(q){
 
+  function createPrompt(){
+    debugger;
+    
+  }
 
+  function assessGuess(){
+    console.log("assessGuess")
+    if(userGuess == currentAnswer){
+      rightAnswer = "true"
+    }
   }
 
   function setScore(){
-
-
+    console.log("setScore")
+  if(rightAnswer = "true"){
+    userScore++
+    }
   }
 
   function getFinalScore(){
-
-
+    console.log("getFinalScore")
   }
 
-  startButton = getElementById("start_button")
+  var startButton = document.getElementById("start_button")
   startButton.addEventListener("click", runQuiz())
-
-
 
 };
 
