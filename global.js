2ac6a42463a75357916f3d63686c33efe608e86e
Kyle Sandage <ksandage08@gmail.com>
2016-03-13 11:11:41 -0500

    trying to loop through questions with next button

--- global.js
+++ global.js
@@ -1,7 +1,8 @@
 window.onload = function(){
   
   var startButton = document.getElementById("begin_button");
-  var submitButton = document.getElementById("submitter")
+  var submitButton = document.getElementById("submitter");
+  var nextButton = document.getElementById("next");
 
   var correctAnswers = 0;
 
@@ -9,18 +10,12 @@ window.onload = function(){
     questionOne = function(){
       document.getElementById("quiz").innerHTML = "What color are oranges?";
       document.getElementById("choices").innerHTML = "Blue, Red, Pink,Orange";
-
+    },
+    questionTwo = function(){
+      document.getElementById("quiz").innerHTML = 
+        "Who ran the first marathon?";
+      document.getElementById("choices").innerHTML = "Kyle Sandage? Steve Prefontaine? Pheidippides? Usain Bolt?";  
     }
-    // questionTwo = function(){
-    //   responseTwo = prompt("Who ran the first marathon? Kyle Sandage? Steve Prefontaine? Pheidippides? Usain Bolt?");
-    //   if (responseTwo.toLowerCase() === "pheidippides"){
-    //     alert("You are correct!");
-    //     correctAnswers += 1;
-    //   }
-    //   else{
-    //     alert("WRONG!");
-    //   }
-    // },
     // questionThree = function(){
     //   responseThree = prompt("What is the capitol of Kansas? Wichita? Manhattan? Lawrence? Kansas City?");
     //   if (responseThree.toLowerCase() === "wichita"){
@@ -33,11 +28,39 @@ window.onload = function(){
     // },
   ];
 
+  var allAnswers = [
+    answerOne = function(){
+      responseOne = document.getElementById("answer").value;
+      if (responseOne.toLowerCase() === "orange"){
+        document.getElementById("question_result").innerHTML = "Correct!";
+        correctAnswers += 1;
+      }
+      else{
+        document.getElementById("question_result").innerHTML = "Wrong!";
+      };  
+    },
+    answerTwo = function(){
+      responseTwo = document.getElementById("answer").value;
+      if (responseTwo.toLowerCase() === "pheidippides"){
+        document.getElementById("question_result").innerHTML = "Correct!";
+        correctAnswers += 1;
+      }
+      else{
+        document.getElementById("question_result").innerHTML = "Wrong!";
+      };
+    }
+  ];
+
   beginGame = function(){
     for(var i = 0; i < allQuestions.length; i++) {
       allQuestions[i]();
     };
+    nextQuestion = function(){
+      i++;
+    };
   };
+
+
 
   // var score = (correctAnswers / allQuestions.length) * 100;
   // alert("You answered " + correctAnswers + " out of " + 
@@ -46,18 +69,14 @@ window.onload = function(){
   // };
 
   submitAnswer = function(){
-    responseOne = document.getElementById("answer").value;
-   if (responseOne.toLowerCase() === "orange"){
-      document.getElementById("question_result").innerHTML = "Correct!";
-      correctAnswers += 1;
-    }
-    else{
-      document.getElementById("question_result").innerHTML = "Wrong!";
-    }
+    for(var i = 0; i < allAnswers.length; i++) {
+      allAnswers[i]();
+    };
   };
 
   submitButton.addEventListener("click", submitAnswer);
   startButton.addEventListener("click", beginGame); 
+  nextButton.addEventListener("click", nextQuestion);
 
 };
 
