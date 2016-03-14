e02d02fce8bb59221cd90149cc1759455c2e73fa
Bradley Biehl <bradley.biehl@gmail.com>
2016-03-14 16:13:36 -0500

    click to start the quiz, first question appears

--- global.js
+++ global.js
@@ -1,9 +1,11 @@
 window.onload = function(){
 
   // when the page loads, the first question's content is added into div#question
-  var click_it = document.getElementById("begin");
-  click_it.addEventListener("click", function() {
-
+  var click_to_begin = document.getElementById("button_begin");
+  click_to_begin.addEventListener("click", function() {
+    var show_quiz = document.getElementById("quiz");
+    show_quiz.style.display = "block";
+    click_to_begin.style.display = "none";
   });
 
 
