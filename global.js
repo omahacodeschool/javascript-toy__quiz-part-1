77e6bcf5bdf8771919bc4e52bda637c7d2085af8
Courtney Burns <courtneylynnburns@gmail.com>
2016-03-14 17:05:26 -0500

    Found out for/in loop was running before the page loaded. Unable to figure out why. Took out loop and using strings for options instead.

--- global.js
+++ global.js
@@ -9,46 +9,26 @@ function set_Question(){
   this.question = "";
   this.options = {};
   this.answer = "";
-}
+};
 
 var q1 = new set_Question();
   q1.question = "What is the name for a group of flamingos?";
-  q1.options = {
-    a: "Flock",
-    b: "Flamboyance",
-    c: "Pink Out", 
-    d: "Gaggle"
-  };
+  q1.options = "a: Flock\nb: Flamboyance\nc: Pink Out\nd: Gaggle";
   q1.answer = "a";
 
 var q2 = new set_Question();
   q2.question = "Which ocean claimed the Titanic?";
-  q2.options = {
-    a: "The Arctic",
-    b: "The East Indian",
-    c: "The South Pacific",
-    d: "The North Atlantic",
-  };
+  q2.options = "a: The Arctic\nb: The East Indian\nc: The South Pacific\nd: The North Atlantic";
   q2.answer = "d";
 
 var q3 = new set_Question();
   q3.question = "How many paintings did vanGogh sell during his lifetime?";
-  q3.optons = {
-    a: 2,
-    b: 27,
-    c: 1,
-    d: 397
-  };
+  q3.options = "a: 27\nb: 789\nc: 1\nd: 2";
   q3.answer = "c";
 
 var q4 = new set_Question();
   q4.question = "Which direction does the Great Pyramid of Giza face?";
-  q4.options = {
-    a: "North",
-    b: "South",
-    c: "East",
-    d: "West"
-  };
+  q4.options = "a: North\nb: South\nc: East\nd: West";
   q4.answer = "a";
 
 var quizQuestions = [q1, q2, q3, q4]
@@ -58,44 +38,34 @@ var quizQuestions = [q1, q2, q3, q4]
 function checkAnswer(submitted_answer, current_questions_answer){
   if (submitted_answer == current_questions_answer){
     alert("Thats correct!")
-    score++
+    score++;
   }else{
     alert("Thats incorrect.")
   }
-} 
+}; 
 
 //This method averages the score into a percentage
 function averageScore(score, questions_array){
   var avg = score/quizQuestions.length
   var avgPercent = avg.toFixed(2)
-}
+};
 
 
-// for/in loop that works for one questions options
-for (var choice in q1.options){
-console.log("These are Q1's options. " + choice + ":" + q1.options[choice] );
-  }
+// // for/in loop that works for one questions options
+// for (var choice in q1.options){
+// console.log("These are Q1's options. " + choice + ":" + q1.options[choice] );
+//   }
 
 
 
 
 // This method runs the quiz
-function quizFunction(questions_array){
+function quizFunction(){
   for (i = 0; i < quizQuestions.length; i++)
 
-    // for/in loop pushes options to empty string for one question
-    var options_string = ""
+    prompt((quizQuestions[i].question) + "\n" + (quizQuestions[i].options), "answer goes here");
 
-    for (var letter in quizQuestions[i].options){
-      options_string += letter; 
-      options_string += ": ";
-      options_string += quizQuestions[i].options[letter];
-      options_string += "\n";
-    }
-
-
-    prompt((quizQuestions[i].question) + "\n" + "options go here", "answer goes here")
-}
+};
 
 
 
@@ -107,6 +77,6 @@ window.onload = function(){
   // debugger;
 
   var start_quiz = document.getElementById("begin_button");
-  start_quiz.addEventListener("click", quizFunction(quizQuestions));
+  start_quiz.addEventListener("click", quizFunction);
     
 };
