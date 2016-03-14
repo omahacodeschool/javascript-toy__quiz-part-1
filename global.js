a310b530b175b27775b08d6f8facc17f3735b90e
Bradley Biehl <bradley.biehl@gmail.com>
2016-03-13 21:29:30 -0500

    experimenting with refactoring

--- global.js
+++ global.js
@@ -1,3 +1,11 @@
 window.onload = function(){
-  alert("Foo");
+
+  var click_it = document.getElementById("begin_button");
+
+  function(correct) {alert("That is correct.")};
+
+  click_it.addEventListener("click", function() {
+    alert("begin the game")
+  });
+
 };
