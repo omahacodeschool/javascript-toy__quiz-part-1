cf7b9e471b7f6e0e8702966772aca54f80fd93f9
Beth Haubert <bethany.haubert@gmail.com>
2016-03-14 08:49:10 -0500

    done

--- global.js
+++ global.js
@@ -9,51 +9,81 @@ window.onload = function(){
   var begin_button = document.getElementById("begin_button");
   var question_text = document.getElementById("question");
   var choices = document.getElementById("choices");
-  var answer = document.getElementById("answer");
+  // var answer = document.getElementById("answer").value;
   var submit_button = document.getElementById("submitter");
   var next_button = document.getElementById("next");
   var result = document.getElementById("total_result");
   var correct_answers = 0;
+  var question_number = 0
 
-
-  begin_button.addEventListener("click", function(){
+ console.log("Adding event listener to begin_button...");
+ begin_button.addEventListener("click", function(){
+    console.log("Heard a 'click' on the begin_button!");
 
     begin_button.style.display = "none";
     quiz.style.display = "block";
 
-    // for (var key in hashQuestionsAndAnswers){
-    //   question_text.innerHTML = key;
-    //   choices.innerHTML = hashQuestionsAndAnswers[key];
-    //   break;
+    var hash_key = Object.keys(hashQuestionsAndAnswers)[question_number];
+    console.log("hash_key is " + hash_key);
 
-    var hash_key = Object.keys(hashQuestionsAndAnswers)[0];
-      question_text.innerHTML = hash_key;
-      
-      choices.innerHTML = hashQuestionsAndAnswers[hash_key].sort(function() { return 0.5 - Math.random() });
+    question_text.innerHTML = hash_key;
 
-      submit_button.addEventListener("click", function(){
+    console.log("hashQuestionsAndAnswers[hash_key] is " + hashQuestionsAndAnswers[hash_key]);
+    choices.innerHTML = hashQuestionsAndAnswers[hash_key]
 
-        if (answer == hashQuestionsAndAnswers[key][0]){
-          next_button.addEventListener("click", function(){
-            alert("You answered correctly");
-            correct_answers += 1 ;
-          });
-        }
+  });
 
-        else {
-          next_button.addEventListener("click", function(){
-            alert("WRONG. The correct answer is " + hashQuestionsAndAnswers[key][0]);
-            });
-        };
+    console.log("Adding event listener for submit_button...");
+    submit_button.addEventListener("click", function(){
 
-      });
-      
+      hash_key = Object.keys(hashQuestionsAndAnswers)[question_number];
+      console.log("Heard a 'click' for on the submit_button!");
+      var answer = document.getElementById("answer").value;
+
+      if (answer == hashQuestionsAndAnswers[hash_key][0]){
+          alert("You answered correctly");
+          correct_answers += 1;
+          question_number += 1;
+
+          console.log("correct_answers is " + correct_answers);
+          console.log("question_number is " + question_number);
+      }
+
+      else {
+          console.log("hashQuestionsAndAnswers[hash_key][0] is " + hashQuestionsAndAnswers[hash_key][0]);
+          alert("WRONG. The correct answer is " + hashQuestionsAndAnswers[hash_key][0]);
+          question_number += 1;
+          console.log("question_number is " + question_number);
+      }
+
     });
 
-    // result.innerHTML = ("You got" + correct_answers + "out of" + Object.keys(hashQuestionsAndAnswers).length + "answers correct");
-    // result.display = "block";
 
-  };
+  console.log("Adding event listenerfor next_button...");
+  next_button.addEventListener("click", function(){
+    console.log("Heard a 'click' on next_button!");
+
+    var hash_key = Object.keys(hashQuestionsAndAnswers)[question_number];
+    console.log("hash_key is " + hash_key);
+
+        if (hash_key != undefined) {
+          question_text.innerHTML = hash_key;
+
+          console.log("hashQuestionsAndAnswers[hash_key] is " + hashQuestionsAndAnswers[hash_key]);
+          choices.innerHTML = hashQuestionsAndAnswers[hash_key];
+        }
+
+          else if (hash_key == undefined) {
+
+          result.innerHTML = ("You got" + correct_answers + "out of" + Object.keys(hashQuestionsAndAnswers).length + "answers correct");
+            result.display = "block";
+
+          }
+
+   
+  });
+
+};
 
 
 
