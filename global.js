705666eebc6b0b6d2beecfada018823bf35108b3
Bradley Biehl <bradley.biehl@gmail.com>
2016-03-14 17:31:33 -0500

    javascript answers added

--- global.js
+++ global.js
@@ -1,7 +1,51 @@
 window.onload = function(){
 
   // when the page loads, the first question's content is added into div#question
+  var questions = new Array();
+  // Moved from Omaha to Lancaster, later renamed Lincoln.
+  questions[0] = "Where was the Nebraska state capital moved to on March 1st, 1867?";
+  // Nebraska was once called "The Great American Desert".
+  questions[1] = "Nebraska was once called what?";
+  // In 1927, Edwin E. Perkins of Hastings invented the powered soft drink Kool-Aid.
+  questions[2] = "In what Nebraska city did Edwin E. Perkins invent Kool-Aid?";
+  // J. Sterling Morton founded Arbor Day in Nebraska City in 1872.
+  questions[3] = "In what Nebraska city was Arbor Day founded?";
+  // The state nickname used to be the "Tree Planter's State", but was changed in 1945 to the "Cornhusker State".
+  questions[4] = "What was Nebraska's nickname prior to 1945?";
+  // State insect is the honeybee.
+  questions[5] = "What is Nebraska's state insect?";
+  // State motto: Equality before the law.
+  questions[6] = "What is Nebraska's state motto?";
+  // The goldenrod was declared the state flower on April 4, 1895.
+  questions[7] = "What is Nebraska's state flower?";
+  // Marlon Brando's mother gave Henry Fonda acting lessons at the Omaha Community Playhouse.
+  questions[8] = "To whom did Marlon Brando's mother give acting lessons at the Omaha Community Playhouse?";
+  // Sidney, Nebraska was the starting point of the Black Hills Gold Rush.
+  questions[9] = "What Nebraska community was the starting point of the Black Hills Gold Rush?";
 
+  var choices = new Array();
+  choices[0] = ["a) Lincoln", "b) Bellevue", "c) Lancaster", "d) Omaha"];
+  choices[1] = ["a) The Bugeater State", "b) The Great American Desert  ", "c) The Wooly Mammoth Capital of the World", "d) Where the West Begins"];
+  choices[2] = ["a) Hastings ", "b) Scottsbluff ", "c) Omaha ", "d) Lincoln"];
+  choices[3] = ["a) Chadron", "b) Omaha ", "c) Lincoln ", "d) Nebraska City"];
+  choices[4] = ["a) The Cornhusker State ", "b) The Beef State ", "c) The Cowboy State ", "d) The Tree Planter's State"];
+  choices[5] = ["a) honeybee", "b) grasshopper ", "c) beetle ", "d) corn borer"];
+  choices[6] = ["a) Go Big Red", "b) Equality Before the Law ", "c) Freedom and Equality ", "d) Liberty and Freedom"];
+  choices[7] = ["a) Lilly", "b) Peony ", "c) Goldenrod ", "d) Sunflower"];
+  choices[8] = ["a) Fred Astaire", "b) Henry Fonda ", "c) Richard Gere ", "d) Nick Nolte"];
+  choices[9] = ["a) Omaha", "b) Sidney ", "c) North Platte", "d) Bellevue"];
+
+  var answers = new Array();
+  answers[0] = ["c"];
+  answers[1] = ["b"];
+  answers[2] = ["a"];
+  answers[3] = ["d"];
+  answers[4] = ["d"];
+  answers[5] = ["a"];
+  answers[6] = ["b"];
+  answers[7] = ["c"];
+  answers[8] = ["b"];
+  answers[9] = ["b"];
 
 
 };
