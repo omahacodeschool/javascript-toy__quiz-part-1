f16e3ce3dafeb91626f1bb76c591ffb5c55dce90
Derek Pressnall <derekpressnall@yahoo.com>
2016-03-14 16:25:16 -0500

    functioning game except the input field doesnt clear itself after the submit event happens.

--- global.js
+++ global.js
@@ -3,8 +3,8 @@ window.onload = function(){
   //Variables 
   count = 0;
   score = 0;
-  question_list = ["Question 1", "Question 2", "Question 3"];
-  choice_list = ["1. A\n 2. B\n", "1. C\n 2. D\n", "1. E\n 2. F\n"];
+  question_list = ["What is my favorite color?", "What's my lucky number?", "Which sounds best to me"];
+  choice_list = ["1. Orange\n 2. Blue\n 3. Yellow\n 4. Green", "1. 1\n 2. 33\n 3. 100\n 4. 23", "1. Laughing\n 2. Thunderstorm\n 3. Crunch of snow\n 4. Guitar"];
   answer_list = ["1", "2", "3"];
 
   //Beginning question
@@ -13,21 +13,17 @@ window.onload = function(){
   var choices = document.getElementById("choices");
   choices.innerText = (choice_list[0]); 
   var question_result = document.getElementById("question_result");
-  question_result.innerText = (answer_list[0]); count++;
+  question_result.innerText = (answer_list[0]);  
 
   //Next button
-  var next = document.getElementById("next")
+  var next = document.getElementById("next");
 
   next.addEventListener("click", function(){
-  
+    
     var questions = document.getElementById("questions");
     questions.innerText = (question_list[count]);
     var choices = document.getElementById("choices");
-    choices.innerText = (choice_list[count]); count++;
-
-    if(count === question_list.length) {
-        alert("Thanks for playing! You got " + score + " out of " + count + " right!");
-    }
+    choices.innerText = (choice_list[count]); 
   });
 
   //Submit button
@@ -36,8 +32,16 @@ window.onload = function(){
   button.addEventListener("click", function(){
     var guess = document.getElementById("answer").value;
     
-    if (guess == answer_list[0]) {
-     score++; 
+    if (guess == answer_list[count]) {
+      alert("Correct!"); score++;
+    } else {
+      alert("incorrect"); 
+    }
+    
+    count++;
+
+    if(count == question_list.length) {
+        alert("Thanks for playing! You got " + score + " out of " + count + " right!");
     }
   });
 };
