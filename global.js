944d924953e026a60401891ac439a8eaf5b99aaa
Courtney Burns <courtneylynnburns@gmail.com>
2016-03-14 18:24:55 -0500

    Working on phase 2 for some reason my listener is running when the page loads...

--- global.js
+++ global.js
@@ -13,94 +13,69 @@ function set_Question(){
 
 var q1 = new set_Question();
   q1.question = "What is the name for a group of flamingos?";
-  q1.options = "a: Flock\nb: Flamboyance\nc: Pink Out\nd: Gaggle";
+  q1.options = "a: Flock <br> b: Flamboyance <br> c: Pink Out <br> d: Gaggle";
   q1.answer = "b";
 
 var q2 = new set_Question();
   q2.question = "Which ocean claimed the Titanic?";
-  q2.options = "a: The Arctic\nb: The East Indian\nc: The South Pacific\nd: The North Atlantic";
+  q2.options = "a: The Arctic <br> b: The East Indian <br> c: The South Pacific<br> d: The North Atlantic";
   q2.answer = "d";
 
 var q3 = new set_Question();
   q3.question = "How many paintings did vanGogh sell during his lifetime?";
-  q3.options = "a: 27\nb: 789\nc: 1\nd: 2";
+  q3.options = "a: 27 <br> b: 789 <br> c: 1 <br> d: 2";
   q3.answer = "c";
 
 var q4 = new set_Question();
   q4.question = "Which direction does the Great Pyramid of Giza face?";
-  q4.options = "a: North\nb: South\nc: East\nd: West";
+  q4.options = "a: North <br> b: South <br> c: East <br> d: West";
   q4.answer = "a";
 
-var quizQuestions = [q1, q2, q3, q4]
-
-
-
+var quizQuestions = [q1, q2, q3, q4];
 
 //This method averages the score into a percentage
 function averageScore(score){
-  var avg = score/quizQuestions.length
-  var avgPercent = avg.toFixed(2)
-  alert("You scored an average of " + avgPercent + " %!")
+  var avg = score/quizQuestions.length;
+  var avgPercent = avg.toFixed(2);
+  alert("You scored an average of " + avgPercent + " %!");
 };
 
 
 
 // This method runs the quiz
 function quizFunction(){
-  var score = 0;
-  submitted = prompt((quizQuestions[0].question) + "\n" + (quizQuestions[0].options), "answer goes here");
+  div_question = document.getElementById("question");
+  div_question.innerHTML = q1.question;
 
-    if (submitted == quizQuestions[0].answer){
-      alert("Correct");
-      score++;
-    }else{
-      alert("Incorrect");
-    }
-
-  submitted = prompt((quizQuestions[1].question) + "\n" + (quizQuestions[1].options), "answer goes here");
-
-    if (submitted == quizQuestions[1].answer){
-      alert("Correct");
-      score++;
-    }else{
-      alert("Incorrect");
-    }
-
-
-  submitted = prompt((quizQuestions[2].question) + "\n" + (quizQuestions[2].options), "answer goes here");
-
-    if (submitted == quizQuestions[2].answer){
-      alert("Correct");
-      score++;
-    }else{
-      alert("Incorrect");
-    }
-
-
-  submitted = prompt((quizQuestions[3].question) + "\n" + (quizQuestions[3].options), "answer goes here");
-
-    if (submitted == quizQuestions[3].answer){
-      alert("Correct");
-      score++;
-    }else{
-      alert("Incorrect");
-    }
-
-
-    averageScore(score)
-
+  div_choices = document.getElementById("choices");
+  div_choices.innerHTML = q1.options;
 };
 
 
-
-
-
-
+//This method checks to see if the users answer is a match to the correct answer
+function checkAnswer(submitted_answer){
+  // div_result = document.getElementById("question_result");
+  
+  // if (submitted_answer == q1.answer){
+  //   div_result.innerHTML = "Correct";
+  // }else{
+  //   div_result.innerHTML = "Incorrect";
+  // }
+  alert("yay i waited til you clicked me!")
+};
 
 window.onload = function(){
   // debugger;
 
   var start_quiz = document.getElementById("begin_button");
   start_quiz.addEventListener("click", quizFunction);
-    
+  
+  var submitted = document.getElementById("submitter");
+  submitted.addEventListener("click", checkAnswer(submitted));
+  
+
 };
+
+
+
+
