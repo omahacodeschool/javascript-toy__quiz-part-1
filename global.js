74b2418d0608fd88825e6bda50ee71c23ad04647
Jillian Waggoner <jillianwaggoner@icloud.com>
2016-03-14 13:52:04 -0500

    Going back to phase 1. Everything was too messed up and broken.

--- global.js
+++ global.js
@@ -1,50 +1,56 @@
-
 window.onload = function(){
+ 
+  var firstQuestionAns = "New York City"
+  var secondQuestionAns = "Stomach"
+  var thirdQuestionAns = "Lake Ontario"
+  var fourthQuestionAns = "Thirty Helens"
+  var correctAns = 0
 
-  count = 0
+  var button = document.getElementById("begin_button");
 
-  for (var i = 0; i < question.length; i++) {
-    var question = document.getElementsByClassName("question");
-    question[i].innerHTML;
-    var choices = document.getElementsByClassName("choices");
-    choices[i].innerHTML;
-  }
+  button.addEventListener("click", function(){
 
-  // var next = document.getElementById("next")
-  // next.addEventListener("click" function(){
-  //   var question = document.getElementById("question");
-  // });
+    var alert_to_show = alert("Quiz time!");
 
-  // var button = document.getElementById("submitter");
-
-  // button.addEventListener("click", function(){
-  //   var answers = document.form("answer").submit("text");
-    
-  //   if (answers == correct answer ) {
-  //     results.innerText = "correct";
-  //   }
-  // });
-
-  // var results = document.getElementById("question_result");
-  // for (var r = 0; r < results.length; i++) {
-  //  if (results[r] == "correct"); count++;
-  // }
-    
+    var firstQuestion = prompt("What was the first city to be illuminated with electric street lamps in 1882? \n\n Tokyo \n London \n New York City \n Buenos Aires");
+    if (firstQuestion === firstQuestionAns){ 
+      alert("That's correct! Click OK to continue."); correctAns++;
+    };
+    if (firstQuestion != firstQuestionAns){
+      alert("Sorry, that's incorrect. Click OK to continue.");
+    };
+    var secondQuestion = prompt("What part of a rat's body can detect a bitter taste? \n\n Feet \n Stomach \n Tongue \n Nose");
+    if (secondQuestion === secondQuestionAns){ 
+      alert("That's correct! Click OK to continue."); correctAns++;
+    };
+    if (secondQuestion != secondQuestionAns){
+      alert("Sorry, that's incorrect. Click OK to continue.");
+    };
+    var thirdQuestion = prompt("What name did the British restore to Lake Frontenac, after the French surrendered it in 1763? \n\n Windermere \n Bassenthwaite Lake \n Lake Victoria \n Lake Ontario");
+    if (thirdQuestion === thirdQuestionAns){ 
+      alert("That's correct! Click OK to continue."); correctAns++;
+    };
+    if (thirdQuestion != thirdQuestionAns){
+      alert("Sorry, that's incorrect. Click OK to continue.");
+    };
+    var fourthQuestion = prompt("How many Helens agree on a variety of opinions in the Kids in the Hall sketch? \n\n Thirty Helens \n Ten Helens \n One Hundred Helens \n Twenty Helens");
+    if (fourthQuestion === fourthQuestionAns){ 
+      alert("That's correct! Click OK to continue."); correctAns++;
+    };
+    if (fourthQuestion != fourthQuestionAns){
+      alert("Sorry, that's incorrect. Click OK to continue.");
+    };
+    if (correctAns == 1){
+      alert("You answered 1 out of 4 correctly!");
+    }
+    if (correctAns == 2){
+      alert("You answered 2 out of 4 correctly!");
+    }
+    if (correctAns == 3){
+      alert("You answered 3 out of 4 correctly!");
+    }
+    if (correctAns == 4){
+      alert("You answered 4 out of 4 correctly!");
+    }
+  });
 };
-
-
-    // if (correctAns == 0){
-    //   alert("You answered 0 out of 4 correctly!");
-    // }
-    // if (correctAns == 1){
-    //   alert("You answered 1 out of 4 correctly!");
-    // }
-    // if (correctAns == 2){
-    //   alert("You answered 2 out of 4 correctly!");
-    // }
-    // if (correctAns == 3){
-    //   alert("You answered 3 out of 4 correctly!");
-    // }
-    // if (correctAns == 4){
-    //   alert("You answered 4 out of 4 correctly!");
-    // }
