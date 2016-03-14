399512b9a2914a6af877e4b2a906013ed46bb8c8
Bradley Biehl <bradley.biehl@gmail.com>
2016-03-14 14:50:58 -0500

    Phase 1 functional, but guilty of crimes against DRY

--- global.js
+++ global.js
@@ -4,38 +4,41 @@ window.onload = function(){
 
   click_it.addEventListener("click", function() {
     alert("begin the game")
+    var score = 0;
+    var questions = 0;
 
     var userAns1 = prompt("What is the capital of Nebraska? a) Lincoln b) Omaha c) Bellevue d) Grand Island");
+    questions += 1
     if (userAns1 != "a") {
       alert("Wrong!")
-      var q1 = 0
     }
     else {
       alert("Correct!")
-      var q1 = 1
+      score += 1
     };
 
     var userAns2 = prompt("Which country won the 2014 World Cup?  a) Spain b)Germany c) Italy d) Brazil");
+    questions += 1
     if (userAns2 != "b") {
       alert("Wrong")
-      var q2 = 0
     }
     else {
       alert("Correct!")
-      var q2 = 1
+      score += 1
     };
 
     var userAns3 = prompt("Which club won the 2012 UEFA Champions League? a)Bayern Munich b) Barcelona c) Chelsea d) Borussia Dortmund");
+    questions += 1
     if (userAns3 != "c") {
       alert("Wrong")
-      var q3 = 0
     }
     else {
       alert("Correct!")
-      var q3 = 1
+      score += 1
     };
 
-    var correct = [q1, q2, q3].sum
+    alert("You got " + score + " out of " + questions + " correct for a score of " + ((score/questions) * 100) + "%");
+
 
 
   });
