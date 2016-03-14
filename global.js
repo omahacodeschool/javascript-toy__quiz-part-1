ddd53d136322dd77547dfb197416786b63d988fd
Chris Foral <chris.foral@gmail.com>
2016-03-14 00:04:09 -0500

    Question Array of Hashes working. Found a function online to access Hash keys by their values

--- global.js
+++ global.js
@@ -3,29 +3,39 @@ window.onload = function(){
 
   var click_it    = document.getElementById("begin_button");
   var questionArr = [{"Which is the superior 80's sequel?": "qOne", 
-                    "Aliens": "correct", "The Road Warrior": "aB", 
-                      "The Empire Strikers Back": "aC", "Evil Dead II": "aD"},
+                    "Aliens": "correct", "The Road Warrior": "B", 
+                      "The Empire Strikers Back": "C", "Evil Dead II": "D"},
                       
-                    {"What is the best color?": "qTwo", "Red": "aA",
-                      "Blue": "aB", "Yellow": "aC", "Green": "correct"},
+                    {"What is the best color?": "qTwo", "Red": "A",
+                      "Blue": "B", "Yellow": "C", "Green": "correct"},
                       
-                    {"Which direction?": "qThree", "East": "aA", "West": "aB", "North": "correct", "South": "aD"},
+                    {"Which direction?": "qThree", "East": "A", "West": "B", "North": "correct", "South": "D"},
                       
-                    {"What is the correct answer?": "qFour", "A": "aA", 
-                      "B": "correct", "C": "aC", "D": "aD"}
+                    {"What is the correct answer?": "qFour", "A": "A", 
+                      "B": "correct", "C": "C", "D": "D"}
                     ]
+
+Object.prototype.getKeyByValue = function( value ) {
+    for( var prop in this ) {
+        if( this.hasOwnProperty( prop ) ) {
+             if( this[ prop ] === value )
+                 return prop;
+        }
+    }
+}
 
   click_it.addEventListener("click", function(){
     // Create a prompt with a 4-answer multiple-choice question
     for (var i = 0; i <= questionArr.length; i++) {
       
       var UserAnswer = prompt(Object.keys(questionArr[i]));
-
-      if (UserAnswer === () {
+// Must figure out how to access the key with the value of "correct" to compare with the UserAnswer
+      if (UserAnswer === questionArr[i].getKeyByValue("correct")) {
         alert("Correct! " + UserAnswer + " is right!")
-        console.log(UserAnswer,)
+        console.log(UserAnswer)
       } else {
         alert("Try again...")
+        console.log(UserAnswer)
       }
     }
     // Create an alert for correct or incorrect
