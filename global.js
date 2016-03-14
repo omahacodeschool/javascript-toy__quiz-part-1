1534fe587fa2c9c829cb4433e7b9d655bdc4a682
Courtney Burns <courtneylynnburns@gmail.com>
2016-03-14 14:11:23 -0500

    loop is working for questions

--- global.js
+++ global.js
@@ -1,3 +1,10 @@
+  //when begin button is clicked start_quiz function is ran
+  //start quiz function runs the following functions:
+  //a loop through each question
+  //a function to compare users answer to correct answer
+  //a function to increase the score of the user if their answer is correct
+
+
 function set_Question(){
   this.question = "";
   this.options = {};
@@ -5,99 +12,77 @@ function set_Question(){
 }
 
 var q1 = new set_Question();
-  q1.question = "What is the name for a group of flamingos?"
+  q1.question = "What is the name for a group of flamingos?";
   q1.options = {
     a: "Flock",
     b: "Flamboyance",
     c: "Pink Out", 
     d: "Gaggle"
-  }
-  q1.answer = "a"
+  };
+  q1.answer = "a";
 
 var q2 = new set_Question();
-  q2.question = "Which ocean claimed the Titanic?"
+  q2.question = "Which ocean claimed the Titanic?";
   q2.options = {
     a: "The Arctic",
     b: "The East Indian",
     c: "The South Pacific",
     d: "The North Atlantic",
-  }
-  q2.answer = "d"
+  };
+  q2.answer = "d";
 
 var q3 = new set_Question();
-  q3.question = "How many paintings did vanGogh sell during his lifetime?"
+  q3.question = "How many paintings did vanGogh sell during his lifetime?";
   q3.optons = {
     a: 2,
     b: 27,
     c: 1,
     d: 397
-  }
-  q3.answer = "c"
+  };
+  q3.answer = "c";
 
 var q4 = new set_Question();
-  q4.question = "Which direction does the Great Pyramid of Giza face?"
+  q4.question = "Which direction does the Great Pyramid of Giza face?";
   q4.options = {
     a: "North",
     b: "South",
     c: "East",
     d: "West"
+  };
+  q4.answer = "a";
+
+var quizQuestions = [q1, q2, q3, q4]
+
+
+
+function checkAnswer(submitted_answer, current_questions_answer){
+  if (submitted_answer == current_questions_answer){
+    alert("Thats correct!")
+  }else{
+    alert("Thats incorrect.")
   }
-  q4.answer = "a"
+} 
 
+function averageScore(score, questions_array){
+  var avg = score/quizQuestions.length
+  var avgPercent = avg.toFixed(2)
+}
 
-function checkAnswer = fucntion(){
-  if (submitted_answer == current_questions_answer)
-    alert("That's correct!")
-  score++
-} else {
-  alert("That is incorrect.")
+function quizFunction(questions_array){
+  for (i = 0; i < quizQuestions.length; i++)
+    prompt(quizQuestions[i].question, "some text")
 }
 
 
 
 
-
-
-
-
 window.onload = function(){
-  
-  var quizFunction = function(){
-
-  var score = 0
-
-  alert("Get ready to be quizzed!");
-
-  if (q1 == a1){
-    score++;
-  }
-
-  if (q2 == a2){
-    score++;
-  }
-
-  if (q3 == a3){
-    score++;
-  }
-
-  if (q4 == a4){
-    score++;
-  }
-
-  var average = score/4
-  alert("You answered " + average.toFixed(2) + "% of the questions correctly!");
-
-  };
-
+  // debugger;
 
   var start_quiz = document.getElementById("begin_button");
-  start_quiz.addEventListener("click", quizFunction);
+  start_quiz.addEventListener("click", quizFunction(quizQuestions));
+  
+
+
+  
 };
-
-
-
-
-
-// what is the name for a flock of flamingos
-// what is the only mammal that lays eggs
-// what controls the ocean tides        
