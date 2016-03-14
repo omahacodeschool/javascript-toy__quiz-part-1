6bff2d98a6fe467d9cac7be3927428a71e726f20
Bradley Biehl <bradley.biehl@gmail.com>
2016-03-14 12:46:34 -0500

    Lost on Phase 2, going back to Phase 1

--- global.js
+++ global.js
@@ -1,19 +1,14 @@
 window.onload = function(){
 
   // Enter total number of questions:
-  var totalquestions=10
+  var totalquestions = 5
 
   // Enter the solutions corresponding to each question:
-  var correctchoices=new Array()
-  correctchoices[1]='c' //question 1 solution
-  correctchoices[2]='a' //question 2 solution, and so on.
-  correctchoices[3]='c'
-  correctchoices[4]='c'
+  var correctchoices = new Array()
+  correctchoices[1]='d' //question 1 solution
+  correctchoices[2]='b' //question 2 solution, and so on.
+  correctchoices[3]='d'
+  correctchoices[4]='a'
   correctchoices[5]='c'
-  correctchoices[6]='c'
-  correctchoices[7]='b'
-  correctchoices[8]='b'
-  correctchoices[9]='c'
-  correctchoices[10]='b'
 
 };
