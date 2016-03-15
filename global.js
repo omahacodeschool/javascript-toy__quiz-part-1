2320d0174676587561ca4a884d03574b1cdd48b1
Bradley Biehl <bradley.biehl@gmail.com>
2016-03-15 09:40:45 -0500

    need to implement if/else for user's answer

--- global.js
+++ global.js
@@ -4,9 +4,9 @@ questions[1] = "Which team won Superbowl XX?"
 questions[2] = "Which club won the UEFA Champions League in 2012?"
 
 var choices = []
-choices[0] = ["a) Omaha", "b) Lincoln", "c) Bellevue", "d) Grand Island"]
-choices[1] = ["a) Chicago Bears", "b) New England Patriots", "c) Miami Dolphins", "d) New York Giants"]
-choices[2] = ["a) Barcelona", "b) Bayern Munich", "c) Chelsea", "d) Manchester United"]
+choices[0] = ["a) Omaha ", "b) Lincoln ", "c) Bellevue ", "d) Grand Island"]
+choices[1] = ["a) Chicago Bears ", "b) New England Patriots ", "c) Miami Dolphins ", "d) New York Giants"]
+choices[2] = "a) Barcelona  b) Bayern Munich  c) Chelsea  d) Manchester United"
 
 var answers = []
 answers[0] = "b"
@@ -16,7 +16,9 @@ answers[2] = "c"
 window.onload = function(){
 
   for (var i = 0; i < questions.length; i++) {
-  document.getElementById("question").innerHTML = questions[i]
+  document.getElementById("question").innerHTML = questions[i];
+  document.getElementById("choices").innerHTML = choices[i];
+  var submit_answer = document.getElementById("submitter");
   }
 
 
