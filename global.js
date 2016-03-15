678508e76de22fb648e392c11e4cbde40d65764c
Thorin Butler <thorin.butler@gmail.com>
2016-03-15 10:30:14 -0500

    Trying to piece it together.  Committing to commit at the moment.

--- global.js
+++ global.js
@@ -1,45 +1,54 @@
 window.onload = function(){
 
-  var correctAnswer = 0
-  var numberOfQuestions = 3
-  var button = document.getElementById("begin_button");
-
-  button.addEventListener("click", function(){
-
-    var questionOne = prompt("What weighs more? \n A)A feather \n B)An elephant \n C)The moon \n D)Nothing");
-
-      if (questionOne == "c"){
-        alert("That's correct!");
-        correctAnswer++;
-      }
-      else {
-        alert("Aww, better luck with the next one.");
-      };
-
-    var questionTwo = prompt("What weighs less? \n A)A bowling ball \n B)A neutron star \n C)A puppy \n D)Your conscience");
-
-      if (questionTwo == "c") {
-        alert("That's correct!");
-        correctAnswer++;
-      }
-      else {
-        alert("Aww, better luck with the next one.");
-      };
-
-    var questionThree = prompt("What weighs on you more? \n A)The atmosphere \n B)Debt \n C)Guilt \n D)Responsibility");
-
-      if (questionTwo == "c") {
-        alert("That's correct!");
-        correctAnswer++;
-      }
-      else {
-        alert("Aww, well, there are other quizzes.");
-      };
-
-      var percentageCorrect = ((correctAnswer/numberOfQuestions) * 100);
-
-      alert("you got " + correctAnswer + " out of " + numberOfQuestions + " correct!  That's good for " + percentageCorrect + "%!");
-
-    });
+  var correctAnswer = 0;
+  var question = document.getElementById("question");
+  var choices = document.getElementById("choices");
+  var answer = document.getElementById("answer");
+  var question_result = document.getElementById("question_result");
+  var next = document.getElementById("next");
+  var 
 
 };
+
+
+  // var correctAnswer = 0
+  // var numberOfQuestions = 3
+  // var button = document.getElementById("begin_button");
+
+  // button.addEventListener("click", function(){
+
+  //   var questionOne = prompt("What weighs more? \n A)A feather \n B)An elephant \n C)The moon \n D)Nothing");
+
+  //     if (questionOne == "c"){
+  //       alert("That's correct!");
+  //       correctAnswer++;
+  //     }
+  //     else {
+  //       alert("Aww, better luck with the next one.");
+  //     };
+
+  //   var questionTwo = prompt("What weighs less? \n A)A bowling ball \n B)A neutron star \n C)A puppy \n D)Your conscience");
+
+  //     if (questionTwo == "c") {
+  //       alert("That's correct!");
+  //       correctAnswer++;
+  //     }
+  //     else {
+  //       alert("Aww, better luck with the next one.");
+  //     };
+
+  //   var questionThree = prompt("What weighs on you more? \n A)The atmosphere \n B)Debt \n C)Guilt \n D)Responsibility");
+
+  //     if (questionTwo == "c") {
+  //       alert("That's correct!");
+  //       correctAnswer++;
+  //     }
+  //     else {
+  //       alert("Aww, well, there are other quizzes.");
+  //     };
+
+  //     var percentageCorrect = ((correctAnswer/numberOfQuestions) * 100);
+
+  //     alert("you got " + correctAnswer + " out of " + numberOfQuestions + " correct!  That's good for " + percentageCorrect + "%!");
+
+  //   });
