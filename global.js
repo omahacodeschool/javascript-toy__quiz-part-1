8805d2bbc627b131e49f958d1bc912cdb55b145c
Tray Harrington <traylon.harrington@gmail.com>
2016-03-15 11:42:12 -0500

    Event Listener and Conditional Working

--- global.js
+++ global.js
@@ -7,18 +7,23 @@ window.onload = function(){
       "The ozone layer restricts:":
       "Ultraviolet radiation", 
 
-      "Eugenics is the study of:";
+      "Eugenics is the study of:":
       "altering human beings by changing their genetic components"
 
   };
 
   var button = document.getElementById("begin_button");
 
-  button.addEventListener("click", function(){
-    //for(var i in hashQA) {
-      //prompt(i);
-      alert("Hi!");
-    //}
+  button.addEventListener("click", function() {
+    for(var i in hashQA) {
+      var answer = prompt(i);
+        if (answer === "a") {
+          alert("Correct!");
+        }else{
+          alert("Incorrect...");
+        }
+  
+    }
   });
 
 };
