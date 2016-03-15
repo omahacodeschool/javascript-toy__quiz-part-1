5433a35eff1148034f586aea74e7cb32afffd54d
Tray Harrington <traylon.harrington@gmail.com>
2016-03-15 10:37:12 -0500

    Q & A Hash created

--- global.js
+++ global.js
@@ -1,3 +1,18 @@
 window.onload = function(){
+  
+  var hashQA = {
+      "Exposure to sunlight helps a person improve his/her health because:": 
+      "the unltraviolet rays convert skin oil into Vitamin D",
+
+      "The ozone layer restricts:":
+      "Ultraviolet radiation", 
+
+      "Eugenics is the study of:";
+      "altering human beings by changing their genetic components"
+
+  };
+
+
+
   alert("Foo");
 };
