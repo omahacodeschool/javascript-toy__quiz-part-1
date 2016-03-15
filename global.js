67c7b7447fc295e65ddc34e480644d0c5bb63d83
Bradley Biehl <bradley.biehl@gmail.com>
2016-03-15 11:37:54 -0500

    now can begin quiz, needed window.onload to capture vars

--- global.js
+++ global.js
@@ -34,51 +34,14 @@ var questions = [
 
 ];
 
-var begin = document.getElementById("begin_button");
-var quiz = document.getElementById("quiz");
+window.onload = function() {
 
-begin.addEventListener("click", function() {
-  quiz.style.display = "block";
-  begin.style.display = "none";
-});
+  var begin = document.getElementById("begin_button");
+  var quiz = document.getElementById("quiz");
 
-var score = 0
-var current_question = 0
+  begin.addEventListener("click", function() {
+    quiz.style.display = "block";
+    begin.style.display = "none";
+  });
 
-
-// begin_button.when_clicked do
-//   begin_button.innerHTML("Next question")
-
-//   current_question = questions[current_question]
-
-//   document.getElementById("question").innerHTML = current_question["content"]
-//   document.getElementById("choices").innerHTML = current_question["answers"].keys
-// end
-
-// submitter.when_clicked do
-//   // See what they typed into 'answer'
-
-//   // Check if that answer is correct.
-//     # If correct, do things.
-//     # If not correct, do other things.
-
-//   // Get user's guess.
-//   user_answer = document.getElementById("answer").value
-
-//   // Check if that answer is set to 'true' in the answers hash.
-//   if current_question["answers"][user_answer] == true
-//     document.getElementById("question_result").innerHTML = "Correct!"
-//     score += 1
-//   else
-//     document.getElementById("question_result").innerHTML = "Wrong!"
-//   end
-
-//   current_question += 1
-
-//   if questions[current_question].nil?
-//     document.getElementById("total_result").innerHTML("Game over. Final score: " + score);
-//   else
-//     // Update the total_result area.
-//     document.getElementById("total_result").innerHTML("So far, " + score + " questions correct.");
-//   end
-// end
+};
