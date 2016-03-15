83d7dd46c0be29faa97e56e324f11243ebbc5690
Bradley Biehl <bradley.biehl@gmail.com>
2016-03-14 20:44:15 -0500

    basic inner HTML

--- global.js
+++ global.js
@@ -1,5 +1,8 @@
-window.onload function() {
-  
+window.onload = function(){
 
+  document.getElementById("question").innerHTML = "Empty for now. We'll use JavaScript to put question text into this.";
+  document.getElementById("choices").innerHTML = "Empty for now. We'll use JavaScript to put choices text into this.";
+  document.getElementById("question_result").innerHTML = "Empty for now. We'll use JavaScript to put each question's result into this.";
+  document.getElementById("total_result").innerHTML = "Empty for now. We'll use JavaScript to put the quiz's final result into this.";
 
 };
