f422ad5efce90f1f6d12383dfb2e5fe43ca92e4f
Bradley Biehl <bradley.biehl@gmail.com>
2016-03-15 09:24:52 -0500

    displaying an questions Array object, not the one I expected

--- global.js
+++ global.js
@@ -15,11 +15,55 @@ answers[2] = "c"
 
 window.onload = function(){
 
+  for (var i = 0; i < questions.length; i++) {
+  document.getElementById("question").innerHTML = questions[i]
+  }
 
 
-  document.getElementById("question").innerHTML = "Empty for now. We'll use JavaScript to put question text into this.";
-  document.getElementById("choices").innerHTML = "Empty for now. We'll use JavaScript to put choices text into this.";
-  document.getElementById("question_result").innerHTML = "Empty for now. We'll use JavaScript to put each question's result into this.";
-  document.getElementById("total_result").innerHTML = "Empty for now. We'll use JavaScript to put the quiz's final result into this.";
+  // document.getElementById("question").innerHTML = "Empty for now. We'll use JavaScript to put question text into this.";
+  // document.getElementById("choices").innerHTML = "Empty for now. We'll use JavaScript to put choices text into this.";
+  // document.getElementById("question_result").innerHTML = "Empty for now. We'll use JavaScript to put each question's result into this.";
+  // document.getElementById("total_result").innerHTML = "Empty for now. We'll use JavaScript to put the quiz's final result into this.";
+
+  // var click_it = document.getElementById("begin_button");
+
+  // click_it.addEventListener("click", function() {
+  //   alert("begin the game")
+  //   var score = 0;
+  //   var questions = 0;
+
+  //   var userAns1 = prompt("What is the capital of Nebraska? a) Lincoln b) Omaha c) Bellevue d) Grand Island");
+  //   questions += 1
+  //   if (userAns1 != "a") {
+  //     alert("Wrong!")
+  //   }
+  //   else {
+  //     alert("Correct!")
+  //     score += 1
+  //   };
+
+  //   var userAns2 = prompt("Which country won the 2014 World Cup?  a) Spain b)Germany c) Italy d) Brazil");
+  //   questions += 1
+  //   if (userAns2 != "b") {
+  //     alert("Wrong")
+  //   }
+  //   else {
+  //     alert("Correct!")
+  //     score += 1
+  //   };
+
+  //   var userAns3 = prompt("Which club won the 2012 UEFA Champions League? a)Bayern Munich b) Barcelona c) Chelsea d) Borussia Dortmund");
+  //   questions += 1
+  //   if (userAns3 != "c") {
+  //     alert("Wrong")
+  //   }
+  //   else {
+  //     alert("Correct!")
+  //     score += 1
+  //   };
+
+  //   alert("You got " + score + " out of " + questions + " correct for a score of " + ((score/questions) * 100) + "%");
+
+  // });
 
 };
