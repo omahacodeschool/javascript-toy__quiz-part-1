300d2af1e7cdac1cf632f39ecae0510936f52b22
Thorin Butler <thorin.butler@gmail.com>
2016-03-15 11:03:10 -0500

    questions and choices appearing for the moment.  Still not entirely sure I understand, but movementis good.

--- global.js
+++ global.js
@@ -6,7 +6,17 @@ window.onload = function(){
   var answer = document.getElementById("answer");
   var question_result = document.getElementById("question_result");
   var next = document.getElementById("next");
-  var 
+  var total_result = document.getElementById("total_result");
+
+  var questionHash = new Object();
+    questionHash["question"] = "As I was going to St. Ives, <br> I met a man with seven wives, <br> Each wife had seven sacks, <br> Each sack had seven cats, <br> Each cat had seven kits: <br> Kits, cats, sacks, and wives, <br> How many were there going to St. Ives?";
+    questionHash["choices"] = ["1, 2402, 343, or 2401"];
+    questionHash["going"] = "1";
+
+  var quiz = document.getElementById('quiz');
+  question.innerHTML = questionHash["question"]
+  choices.innerHTML = questionHash["choices"]
+  quiz.style.display = "block";
 
 };
 
