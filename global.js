2f57d7497bf7dbd787fbc0979963b6e5859504f6
Bradley Biehl <bradley.biehl@gmail.com>
2016-03-14 21:03:16 -0500

    questions, choices, answers added to javascript

--- global.js
+++ global.js
@@ -1,4 +1,21 @@
+var questions = []
+questions[0] = "What is the capital of Nebraska?"
+questions[1] = "Which team won Superbowl XX?"
+questions[2] = "Which club won the UEFA Champions League in 2012?"
+
+var choices = []
+choices[0] = ["a) Omaha", "b) Lincoln", "c) Bellevue", "d) Grand Island"]
+choices[1] = ["a) Chicago Bears", "b) New England Patriots", "c) Miami Dolphins", "d) New York Giants"]
+choices[2] = ["a) Barcelona", "b) Bayern Munich", "c) Chelsea", "d) Manchester United"]
+
+var answers = []
+answers[0] = "b"
+answers[1] = "a"
+answers[2] = "c"
+
 window.onload = function(){
+
+
 
   document.getElementById("question").innerHTML = "Empty for now. We'll use JavaScript to put question text into this.";
   document.getElementById("choices").innerHTML = "Empty for now. We'll use JavaScript to put choices text into this.";
