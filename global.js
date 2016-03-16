412ee667b7b9eb030d2b32012eef8333c5125184
Thorin Butler <thorin.butler@gmail.com>
2016-03-16 11:20:17 -0500

    Phase II working.

--- global.js
+++ global.js
@@ -1,5 +1,6 @@
 window.onload = function(){
 
+  // This loads all our variables.  I think they're fairly self-explanatory.  
   var correctAnswer = 0;
   var numberOfQuestions = 3;
   var current_question_index = 0;
@@ -9,6 +10,7 @@ window.onload = function(){
   var question_result = document.getElementById("question_result");
   var next = document.getElementById("next");
   
+  // Our questions objects are created below.
   var questionHashOne = new Object();
     questionHashOne["question"] = "As I was going to St. Ives, <br> I met a man with seven wives, <br> Each wife had seven sacks, <br> Each sack had seven cats, <br> Each cat had seven kits: <br> Kits, cats, sacks, and wives, <br> How many were there going to St. Ives?";
     questionHashOne["choices"] = ["1, 2402, 343, or 2401"];
@@ -24,18 +26,24 @@ window.onload = function(){
     questionHashThree["choices"] = ["Man, a dog, a mutant, or Steve"];
     questionHashThree["solution"] = "Man";
 
+  // Clicking the button begins the quiz.  
   var button = document.getElementById('begin_button');
+
+  // This creates an array of our question objects.
   var questionsArray = [questionHashOne, questionHashTwo, questionHashThree]
   button.addEventListener("click", function() {
 
+    // This displays our first question on the page.
     var quiz = document.getElementById('quiz');
       question.innerHTML = questionHashOne["question"];
       choices.innerHTML = questionHashOne["choices"];
       quiz.style.display = "block";
 
+    // The blocks below were created to extract the functions from the         .addEventListener contracts to ease readability and, hopefully, provide clarity.  
     var next_block = function(){
+      current_question_index++;  
       question_result.innerHTML = "";
-      answer.value = ""
+      answer.value = "";
       quiz = document.getElementById('quiz');
       question.innerHTML = questionsArray[current_question_index]["question"];
       choices.innerHTML = questionsArray[current_question_index]["choices"];
@@ -50,30 +58,36 @@ window.onload = function(){
       else {
         question_result.innerHTML = "Sorry, that's incorrect."
       };
-    current_question_index++  
     }; 
     
     var total_result_block = function() {
+      if(current_question_index == numberOfQuestions){
+
       quiz.style.display = "none";
       var percentageCorrect = ((correctAnswer/numberOfQuestions) * 100);
-
       var total_result = document.getElementById("total_result");
         total_result.innerHTML = "You got " + correctAnswer + " out of " + numberOfQuestions + " correct!  That's good for " + percentageCorrect + "%!"
         total_result.style.display = "block";
+      };
     };
 
-    submitter.addEventListener("click", question_result_block); 
-    
+    // After the first question is answered and you click submit and this is what follows.
+    submitter.addEventListener("click", question_result_block);
 
     next.addEventListener("click", next_block);
 
     submitter.addEventListener("click", question_result_block);
-    
+
     next.addEventListener("click", next_block); 
-  
+
     submitter.addEventListener("click", question_result_block );
 
     next.addEventListener("click", total_result_block );
+
+  });
+};
+
+
 
   // var correctAnswer = 0
   // var numberOfQuestions = 3
